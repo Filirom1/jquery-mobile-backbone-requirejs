94815b8609bf1f79777dd1181bd3268642818008
Romain <filirom1@gmail.com>
2012-05-21 08:17:52 +0200

    remove comments and cluster from server.js

--- server.js
+++ server.js
@@ -1,35 +1,11 @@
-// Require libraries
-var os = require("os");
-var fs = require("fs");
-var cluster = require("cluster");
-var express = require("express");
-var site = express.createServer();
+var fs = require("fs"),
+  express = require("express"),
+  app = express.createServer();
 
-// Var up, bro
-var i;
+app.use(express.static('client'));
 
-// Master thread spawns new listeners
-if (cluster.isMaster) {
-  // Spawn a new worker for each available thread
-  for (i = 0; i < os.cpus().length; i++) {
-    cluster.fork();
-  }
-
-  return;
-}
-
-// Server static files
-site.use("/css", express.static("client/css"));
-site.use("/js", express.static("client/js"));
-site.use("/templates", express.static("client/templates"));
-
-// API serving will happen here
-// site.use("/api", ...);
-
-// Ensure all routes go home, client side app..
-site.get("*", function(req, res) {
+app.get("*", function(req, res) {
   fs.createReadStream("client/index.html").pipe(res);
 });
 
-// Actually listen
-site.listen(8000);
+app.listen(8000);
